a61c5ded511b3bf63caf4333eca5c98b259ed02d
Rushell White <rushell.white@gmail.com>
2020-10-13 21:56:43 -0400

    Finished the Parser Subclasses try - 3

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -41,11 +41,13 @@ class MarkdownParser:
 
     extensions = [".md", ".markdown"]
 
-    def parse(self, path: Path, source: Path, dest: Path):
+    def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        self.write(path,dest, html)
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name,content))
+        self.write(path, dest, html)
+        sys.stdout.write(
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
+        )
 
 
 class ReStructuredTextParser:
